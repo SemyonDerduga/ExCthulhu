c9166edb6ce3eaea04bef9e901a17987512613f7
semenderduga <semenderduga@gmail.com>
2020-04-20 14:29:00 +0300

    Refactor. Exchanges dict moved

--- cthulhu_src/services/exchange_manager.py
+++ cthulhu_src/services/exchange_manager.py
@@ -1,6 +1,26 @@
-from cthulhu_src.services.exchanges import BaseExchange, get_exchange_by_name
 import asyncio
 import itertools
+
+from cthulhu_src.services.exchanges import *
+
+exchanges_instances = {
+    'binance': Binance,
+    'dsx': Dsx,
+    'exmo': Exmo,
+    'hollaex': Hollaex,
+    'oceanex': Oceanex,
+    'poloniex': Poloniex,
+    'tidex': Tidex,
+    'upbit': Upbit,
+    'yobit': Yobit,
+}
+
+
+def get_exchange_by_name(exchange_name):
+    if exchange_name in exchanges_instances:
+        return exchanges_instances[exchange_name]()
+    else:
+        return GenericExchange(exchange_name)
 
 
 class ExchangeManager:
--- cthulhu_src/services/exchanges/__init__.py
+++ cthulhu_src/services/exchanges/__init__.py
@@ -12,23 +12,6 @@ from .upbit import Upbit
 from .yobit import Yobit
 
 __all__ = ['BaseExchange', 'BatchingExchange', 'Binance', 'Dsx', 'Exmo',
-           'Hollaex', 'Oceanex', 'Poloniex', 'Tidex', 'Upbit', 'Yobit']
-
-exchanges = {
-    'binance': Binance,
-    'dsx': Dsx,
-    'exmo': Exmo,
-    'hollaex': Hollaex,
-    'oceanex': Oceanex,
-    'poloniex': Poloniex,
-    'tidex': Tidex,
-    'upbit': Upbit,
-    'yobit': Yobit,
-}
+           'Hollaex', 'Oceanex', 'Poloniex', 'Tidex', 'Upbit', 'Yobit', 'GenericExchange']
 
 
-def get_exchange_by_name(exchange_name):
-    if exchange_name in exchanges:
-        return exchanges[exchange_name]()
-    else:
-        return GenericExchange(exchange_name)
