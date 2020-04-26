79bbb49f02ba2b27cc607f055ea80d0e32ae0eb9
semenderduga <semenderduga@gmail.com>
2020-04-26 21:16:43 +0300

    Fix help

--- cthulhu_src/routes/find_txn.py
+++ cthulhu_src/routes/find_txn.py
@@ -8,14 +8,14 @@ from cthulhu_src.actions.find_txn import run as run_cmd
 
 HELP_MAX_DEPTH = 'Max depth of transaction exchange and transfer.'
 HELP_START = 'Currency and Exchange Entry Point'
-HELP_ = 'Amount of starting currency'
+HELP_AMOUNT = 'Amount of starting currency'
 
 
 @click.command()
 @click.pass_context
 @click.option('-d', '--max_depth', type=int, default=4, help=HELP_MAX_DEPTH)
 @click.option('-s', '--start', required=True, help=HELP_START)
-@click.option('-a', '--amount', type=float, default=1.0, help=HELP_START)
+@click.option('-a', '--amount', type=float, default=1.0, help=HELP_AMOUNT)
 @click.option('-e', '--exchange_list', multiple=True, default=['yobit', 'binance'])
 def find(ctx, max_depth, start, amount, exchange_list):
     """
