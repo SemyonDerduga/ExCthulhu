e0262ae066bd27fd2f2fc42cce36203ed360c8d5
Semyon Derduga <semenderduga@gmail.com>
2020-05-02 22:10:10 +0300

    Add cashed dir to cli options

--- cthulhu_src/routes/find_txn.py
+++ cthulhu_src/routes/find_txn.py
@@ -12,6 +12,7 @@ HELP_AMOUNT = 'Amount of starting currency'
 HELP_CACHED = 'Cache exchange data'
 HELP_CURRENT_NODE = 'Currency and Exchange where you stopped'
 HELP_CURRENT_AMOUNT = 'Amount of currency where you stop'
+HELP_CACHED_DIR = 'Path to folder with cashed exchange data. Default: ~/.cache/cthulhu '
 
 
 @click.command()
@@ -20,11 +21,12 @@ HELP_CURRENT_AMOUNT = 'Amount of currency where you stop'
 @click.option('-s', '--start', required=True, help=HELP_START)
 @click.option('-a', '--amount', type=float, default=1.0, help=HELP_AMOUNT)
 @click.option('-c', '--cached', type=bool, default=False, help=HELP_CACHED, is_flag=True)
+@click.option('--cache-dir', default='~/.cache/cthulhu', help=HELP_CACHED_DIR)
 @click.option('-e', '--exchange-list', multiple=True, default=['yobit', 'binance'])
 @click.option('--current-node', required=False, help=HELP_CURRENT_NODE)
 @click.option('--current-amount', required=False, type=float, help=HELP_CURRENT_AMOUNT)
-def find(ctx, max_depth, start, amount, cached, exchange_list, current_node, current_amount):
+def find(ctx, max_depth, start, amount, cached, cache_dir, exchange_list, current_node, current_amount):
     """
         Find winning transaction and print it.
     """
-    asyncio.run(run_cmd(ctx, max_depth, exchange_list, start, amount, current_node, current_amount, cached))
+    asyncio.run(run_cmd(ctx, max_depth, exchange_list, start, amount, cache_dir, current_node, current_amount, cached))
