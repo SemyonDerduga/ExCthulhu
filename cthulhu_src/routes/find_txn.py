784ec82805117a2e561eb200184a1bc0cd2a454f
Semyon Derduga <semenderduga@gmail.com>
2020-05-02 20:11:23 +0300

    Fix args _ -> -

--- cthulhu_src/routes/find_txn.py
+++ cthulhu_src/routes/find_txn.py
@@ -16,13 +16,13 @@ HELP_CURRENT_AMOUNT = 'Amount of currency where you stop'
 
 @click.command()
 @click.pass_context
-@click.option('-d', '--max_depth', type=int, default=4, help=HELP_MAX_DEPTH)
+@click.option('-d', '--max-depth', type=int, default=4, help=HELP_MAX_DEPTH)
 @click.option('-s', '--start', required=True, help=HELP_START)
 @click.option('-a', '--amount', type=float, default=1.0, help=HELP_AMOUNT)
 @click.option('-c', '--cached', type=bool, default=False, help=HELP_CACHED, is_flag=True)
-@click.option('-e', '--exchange_list', multiple=True, default=['yobit', 'binance'])
-@click.option('--current_node', required=False, help=HELP_CURRENT_NODE)
-@click.option('--current_amount', required=False, type=float, help=HELP_CURRENT_AMOUNT)
+@click.option('-e', '--exchange-list', multiple=True, default=['yobit', 'binance'])
+@click.option('--current-node', required=False, help=HELP_CURRENT_NODE)
+@click.option('--current-amount', required=False, type=float, help=HELP_CURRENT_AMOUNT)
 def find(ctx, max_depth, start, amount, cached, exchange_list, current_node, current_amount):
     """
         Find winning transaction and print it.
