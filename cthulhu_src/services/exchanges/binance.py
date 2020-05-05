a9878e4905f4627cb1c81567f31f8534e602055c
Oleg Utkin <oleg_utkin@me.com>
2020-05-05 14:01:40 +0300

    change binance limit

--- cthulhu_src/services/exchanges/binance.py
+++ cthulhu_src/services/exchanges/binance.py
@@ -4,7 +4,7 @@ from cthulhu_src.services.exchanges.base_exchange import BaseExchange
 class Binance(BaseExchange):
     name = 'binance'
     opts = {
-        'enableRateLimit': True,
+        # 'enableRateLimit': True,
         'rateLimit': 500,
     }
-    limit = 5000
+    limit = 100
