92ae45a2a5db24cea6bbffd9740b4628d9b63bd4
Semyon Derduga <semenderduga@gmail.com>
2025-08-02 10:33:48 +0300

    Improve web UI usability

--- cthulhu_src/web/routes/pages.py
+++ cthulhu_src/web/routes/pages.py
@@ -7,6 +7,7 @@ from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
 import os
+
 templates_dir = os.path.join(os.path.dirname(os.path.dirname(__file__)), "templates")
 templates = Jinja2Templates(directory=templates_dir)
 router = APIRouter()
@@ -41,7 +42,8 @@ async def integrated_page(request: Request):
     """Страница интегрированного анализа."""
     return templates.TemplateResponse("integrated.html", {"request": request})
 
+
 @router.get("/test", response_class=HTMLResponse)
 async def test_page(request: Request):
     """Страница тестирования API."""
-    return templates.TemplateResponse("test_api.html", {"request": request}) 
+    return templates.TemplateResponse("test_api.html", {"request": request})
